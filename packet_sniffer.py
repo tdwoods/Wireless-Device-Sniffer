f57d72b1edef00f310d9b173be25730c9b748f06
treverw <treverw@evadminsimac2.lan>
2018-07-24 17:05:17 -0400

    Fixed subprocess call

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,11 +1,9 @@
-import subprocess
+import os
 import time
-
-results = open('Results.txt', 'a')
 
 for x in range(2):
     startTime = time.time()
-    blah = subprocess.run(["tshark –n –i wlan0mon -a duration:30 –T fields -e wlan.sa"], stdout=subprocess.PIPE)
-    num_devices = set(blah.stdout.split('\n'))
+    os.system("tshark –n –i wlan0mon -a duration:30 –T fields -e wlan.sa > results.txt")
+    num_devices = open("results.txt", r).read().split("\n")
     results.write(startTime + ': ' + num_devices)
     time.sleep(30)
