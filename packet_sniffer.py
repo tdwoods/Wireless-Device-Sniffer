9b3edda6cf8a23f14b6d5abd984868bfc4e4ae22
treverw <treverw@evadminsimac2.lan>
2018-07-25 16:01:06 -0400

    Hi

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -4,20 +4,23 @@ import sqlite3
 import subprocess
 import time
 
+global continue_running = True
 data = {}
 def packetHandler(packet):
     print("Packet Captured")
     if packet.wlan.ta not in data:
         data[packet.wlan.ta] = packet.radiotap.dbm_antsignal
+    packet.pretty_print()
+    if(data >= 10) continue_running = False
 
-try:
-    print("Starting Capture")
-    capture = pyshark.LiveCapture(interface = 'wlan0mon', bpf_filter = 'type mgt subtype probe-req')
-    capture.sniff_continously(packet_count = 5)
-    capture.apply_on_packets(packetHandler)
-    pkt = capture[0]
-    pkt.pretty_print()
-    print("Done")
-except KeyboardInterrupt:
-    print(str(len(data)) + " Unique Addresses found")
-    print(data)
+while continue_running:
+    try:
+        print("Starting Capture")
+        capture = pyshark.LiveCapture(interface = 'wlan0mon', bpf_filter = 'type mgt subtype probe-req')
+        capture.sniff_continously()
+        capture.apply_on_packets(packetHandler)
+    except KeyboardInterrupt:
+        print(str(len(data)) + " Unique Addresses found")
+        print(data)
+print("Done")
+print(data)
