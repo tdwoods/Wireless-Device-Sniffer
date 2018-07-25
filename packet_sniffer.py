d0fc7f6be29244238775cdc7c898b768a0c0337d
treverw <treverw@evadminsimac2.lan>
2018-07-25 14:27:24 -0400

    Hi

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -10,12 +10,12 @@ def packetHandler(packet):
     if packet.wlan.ta not in addresses:
         addresses.append(packet.wlan.ta)
 
+
 while True:
     try:
         print("Starting Capture")
         capture = LiveCapture(interface = 'wlan0mon', bpf_filter = 'type mgt subtype probe-req')
         capture.sniff(timeout=60)
         capture.apply_on_packets(packetHandler)
-        print(len(addresses) + "Unique Addresses found")
     except KeyboardInterrupt:
-        break
+        print(len(addresses) + "Unique Addresses found")
